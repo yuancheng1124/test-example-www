5af574d14f720c3d80b5b3bb29f9669909c8bb31
Yuan Cheng <cy@cydemacbook-pro.attlocal.net>
2019-03-21 23:29:22 -0400

    add www

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "test_example";
 
-wasm.greet();
+let test = wasm.TestInterface.new();
+let bundle_id = String("test_bundle");
+console.log(bundle_id);
+test.create_bundle(bundle_id);
+let value = test.format(bundle_id, String("key0"));
+console.log(value);
+alert(value);
